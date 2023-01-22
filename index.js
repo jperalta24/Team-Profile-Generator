ac745f15414f0c5bc20846abebb3ecc7875e18ff
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-01-21 20:16:01 -0800

    added engineer prompt

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const promptManager = () => {
             type: 'input',
             message: 'what is your email address? (required)',
             name: 'email',
-            validate: email =>{
+            validate: email => {
                 if (email) {
                     return true;
                 } else {
@@ -61,14 +61,14 @@ const promptManager = () => {
             }
         }
     ])
-    .then(managerInput => {
-        //use the deconstructing syntax to store values from the object properties into a variable
-        const {name, id, email, officeNumber} = managerInput;
-        const manager = new Manager(name, id, email, officeNumber);
-        teamMembers.push(manager);
-        console.log(manager);
-        promptEmployees();
-    })
+        .then(managerInput => {
+            //use the deconstructing syntax to store values from the object properties into a variable
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
+            teamMembers.push(manager);
+            console.log(manager);
+            promptEmployees();
+        })
 }
 
 const promptEmployees = () => {
@@ -76,6 +76,92 @@ const promptEmployees = () => {
     // use a list type to give options for type of employees
     // prompt questions
     // write a conditional for the type of questions to be prompted dependant on the type of employee.
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Please select the next team member you want to add; if finished, select \'finish building my team\'',
+            name: 'teamMembers',
+            choices: ['add engineer to the team', 'add an intern to the team', 'finish building my team'],
+        }
+    ])
+        .then(userChoice => {
+            switch (userChoice.teamMembers) {
+                case 'add an engineer to the team':
+                    promptEngineer();
+                    break;
+                case 'add an intern to the team':
+                    promptIntern();
+                    break;
+                case 'finish building my team':
+                    promptFinishTeam();
+                    break;
+            }
+        })
+}
+
+const promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your engineer\'s name? (required)',
+            name: 'name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'what is your employee id number? (required)',
+            name: 'id',
+            validate: id => {
+                if (isNaN(id)) {
+                    console.log('Please enter your employee id');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'what is your email address? (required)',
+            name: 'email',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address');
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'what is your GitHub username? (required)',
+            name: 'github',
+            validate: github => {
+                if (github) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username');
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(engineerInput => {
+            //use the deconstructing syntax to store values from the object properties into a variable
+            const { name, id, email, github } = managerInput;
+            const engineer = new Engineer(name, id, email, github);
+            teamMembers.push(engineer);
+            console.log(engineer);
+            promptEmployees();
+        })
+
+
 }
 
 
